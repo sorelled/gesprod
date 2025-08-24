5fc9b6c9635fbfe5312601c1fc3f8b3eede4aca3
sorelle D <sorelled039@gmail.com>
2025-08-24 09:51:46 +0100

    feature/03-dto

---
+++ src/main/java/sorelle/gesprod/category/CategoryDTO.java
@@ -0,0 +1,16 @@
+package sorelle.gesprod.category;
+
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryDTO {
+    private UUID id;
+
+    @Size(min = 3, max = 48, message = "{Size.category.nomCategory}")
+    private String nomCategory;
+}
